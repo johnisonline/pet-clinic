07bda66d75f50f326d2e8437f713f98a72244e0b
jmarias <john_mariasoosai_anthony@optum.com>
2019-09-01 18:21:00 -0500

    Refactor: Closes #7 #11 index page and controller created

---
+++ petclinicweb/src/main/java/com/john/petclinic/controller/OwnerController.java
@@ -0,0 +1,14 @@
+package com.john.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owner")
+public class OwnerController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String index(){
+        return "owners/index";
+    }
+}
