044c49e95e7489fce46f2bcb4e686ac5eeacbcbc
jmarias <john_mariasoosai_anthony@optum.com>
2019-09-22 06:42:40 -0500

    Refactor: closes #21 to create Visit entity

--- petclinicdata/src/main/java/com/john/petclinic/model/Owner.java
+++ petclinicdata/src/main/java/com/john/petclinic/model/Owner.java
@@ -1,6 +1,22 @@
 package com.john.petclinic.model;
 
 
+import java.util.List;
+import java.util.Set;
+
 public class Owner extends Person {
 
+    private Set<Pet> pet;
+
+    public Owner(Set<Pet> pet) {
+        this.pet = pet;
+    }
+
+    public Set<Pet> getPet() {
+        return pet;
+    }
+
+    public void setPet(Set<Pet> pet) {
+        this.pet = pet;
+    }
 }
